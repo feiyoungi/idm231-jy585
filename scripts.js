cf382320ee91b3b42ae7a40daf7ae4d781869dd5
feiyoungi <146308282+feiyoungi@users.noreply.github.com>
2024-03-10 18:00:02 -0400

    Add help modal

--- scripts.js
+++ scripts.js
@@ -1,44 +1,11 @@
-const modal = document.getElementById("#modal");
-const charizardButton = document.getElementById("charizard-open");
-const snorlaxButton = document.getElementById("snorlax-open");
-const closeModal = document.querySelector(".close-button");
+const open = document.getElementById ('open');
+const help_modal_container = document.getElementById ('help_modal_container');
+const close = document.getElementById ('close');
 
-// Function to update modal content
-// Add audio
-
-function updateModalContent(title, description, imageSrc) {
-  const modalTitle = modal.querySelector("h2");
-  const modalDescription = modal.querySelector("p");
-  const modalImage = modal.querySelector(".pokemon-image");
-
-  modalTitle.textContent = title;
-  modalDescription.textContent = description;
-  modalImage.src = imageSrc;
-}
-
-// Buttons 
-
-charizardButton.addEventListener("click", () => {
-    const title = "Charizard";
-    const description = "You are confident and ambitious, and you set the trail ablaze behind you wherever you go. You may be occasionally impatient and stubborn- you’re not exactly everyone’s cup of tea- but you are warm-hearted nonetheless.";
-    const imageSrc = "/images/mega_char-min.png";
-    console.log("button-click");
-
-    updateModalContent(title, description, imageSrc);
-    modal.setAttribute('open', 'true'); // Open modal
+open.addEventListener ('click', () => {
+  help_modal_container.classList.add ('show');
 });
 
-snorlaxButton.addEventListener("click", () => {
-    const title = "Snorlax";
-    const description = "You are relaxed and laid-back, preferring to spend your days sleeping and eating. You may come across as lazy to some, but you simply enjoy taking things slow and savoring life’s simple pleasures.";
-    const imageSrc = "/images/snorlax-min.png";
-
-    updateModalContent(title, description, imageSrc);
-    modal.setAttribute('open', 'true'); // Open modal
-});
-
-// Close button 
-
-closeModal.addEventListener("click", () => {
-  modal.style.display = "none";
+close.addEventListener ('click', () => {
+  help_modal_container.classList.remove ('show');
 });
